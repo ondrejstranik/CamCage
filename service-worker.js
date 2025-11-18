5e0ff8a874792a947e8b15bf5dc38ffb486449e0
Ondrej Stranik <101558639+ondrejstranik@users.noreply.github.com>
2025-11-18 10:45:20 +0100

    improved sw

--- service-worker.js
+++ service-worker.js
@@ -18,6 +18,14 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  // Bypass service worker for local device access
+  if (url.hostname === "http://192.168.4.1/") {
+    return; // Let browser fetch normally
+  }
+
+  // Normal PWA caching
   event.respondWith(
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
