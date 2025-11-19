87d79309b716a03f84c1eb78ca0e7fe57d002780
Stranik <ondra.stranik@gmail.com>
2025-11-19 14:55:49 +0100

    pwa corrections

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,9 @@
-const CACHE_NAME = "wifi-check-v1";
+const CACHE_NAME = "CamCage-v1";
 const FILES_TO_CACHE = [
   "index.html",
   "manifest.webmanifest",
   "service-worker.js",
-  "icons/icon-192.png",
-  "icons/icon-512.png"
+  "icons/icon.png"  
 ];
 
 self.addEventListener("install", (event) => {
